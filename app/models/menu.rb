004f72c195d2f5f5b75f66786460e2454cddf31a
cfowler650 <cfowler650@gmail.com>
2019-10-10 10:07:48 -0500

    added destroy to has many relationships to stop orphaned data on delete

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,6 @@
 class Menu < ApplicationRecord
   belongs_to :restaurant
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :reviews, through: :items
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-    has_many :menus
+    has_many :menus, dependent: :destroy
     has_many :items, through: :menus
 end

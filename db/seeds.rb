4f8f38ebe7e1d1f8a3f405fa2e54758169174143
cfowler650 <cfowler650@gmail.com>
2019-10-10 09:41:42 -0500

    restaurantcontext client update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,60 +6,70 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)rest
 
-restaurant = Restaurant.create(name: 'Arbys', cuisine: 'American Fast Food', image: '/images/arbys.jpg')
+restaurant = Restaurant.create(name: 'Holy Pies', cuisine: 'Pizza', image: '/images/pizza.jpg')
 
 menu = Menu.create(name: 'Main', restaurant: restaurant)
 
 item = Item.create(
-        name: 'Sandwich 0',
-        description: 'This sandwich is cheesy, really cheesy. The butter toast melts in your mouth. Comes with pepperjack cheese and roast beef',
-        image: '/images/burger4.jpg',
+        name: 'Pepperoni Pie',
+        description: 'This pie has an incredible aroma when it comes out the oven. The herb and spices in our sauce will make you forget your wearabouts young traveler.',
+        image: '/images/pie1.jpg',
         menu: menu
 )
 
 
 user = User.create(
-        name: 'Billy Kramer'
+        name: 'Amy Storm'
 )
 
 review = Review.create(
-        body: 'Fantastic sandwich, I have never had better!',
+        body: 'This was the best pizza I have ever had in my young life!',
         rating: 5,
         item: item,
         user: user
 )
 
 item2 =  Item.create(
-        name: 'Sandwich 1',
-        description: 'If you love turkey this sandwich is the one for you. It comes with avocado bacon savory garden fresh tomato and mozzarella!',
-        image: '/images/burger5.jpg',
+        name: 'Veggie Pie',
+        description: 'When it comes to pies...well we do it better than just about everybody. This pie is literally to die for. Even the meat eaters will love this one, onions, peppers, cilantro, roma tomatoes are just some of the flavors that will tickle your tongue with this delicious pizza hut favorite.',
+        image: '/images/pie3.jpg',
         menu: menu
 )
 
 
 item3 = Item.create(
-        name: 'Sandwich 2',
-        description: 'A masterpiece, chili peppers, roasted garlic, jalapenos and a secret mustard sauce all on top of honey baked ham slices enveloped in a thick country potato bread home made by Arby himself.',
-        image: '/images/burger.jpg',
+        name: 'The Carnivore',
+        description: 'Think back to your ancient roots with this pie, let the smoldering smoky sausage flirt with your reptilian brain. Let your third eye experience this incredible mix of the highest quality meats there is. Pepperonies, sausages, ham, salami, any meat you can name is on this pie.',
+        image: '/images/pie2.jpg',
         menu: menu
 )
 
 
 
 review2 = Review.create(
-        body: 'Grand, I am really a fan of this ham',
-        rating: 4,
+        body: 'This pie is absolutely phenomenal, I will be taking all my friends here. The waitress was phenomenal as well and never left us hanging on refills. I can not wait to go back, thank you so much holy pie!',
+        rating: 5,
         item: item3,
         user: user
 )
 
 
 review3 = Review.create(
-        body: 'Turkey is not really my favorite but this was tolerable',
-        rating: 3,
+        body: 'Veggie usually is not my thing, but this pizza was next level. I literally fell out of my seat on the first bite. The ranch was killer as well, we even got free sodas with our pies for being first timers!',
+        rating: 5,
         item: item2,
         user: user
 )
+
+
+
+review4 = Review.create(
+        body: 'I am outta here literally, what a joke, what an awful experience, I am going back to pizza hut',
+        rating: 2,
+        item: item2,
+        user: User.first
+)
+
 
 # dish_one = Item.create(
 #     {
